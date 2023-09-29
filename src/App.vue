a4de52258dd419e4c6352ae175d355c40903be6a
Michał Wolny <michal.wolny@it-solve.pl>
2023-09-29 10:09:13 +0200

    zmiany

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,49 @@
   import BarChart from './components/BarChart.vue'
 
   const chartOptions = ref({
-    responsive: true
+    responsive: true,
+    scales: {
+      x: {
+        stacked: true
+      },
+      y: {
+        stacked: true
+      }
+    }
   })
 
   const chartData = ref({
-     labels: [ 'January', 'February', 'March'],
+     labels: [ 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
         datasets: [
           {
-            label: 'Data One',
+            type: 'line',
+            label: 'Line Dataset',
+            data: [40, 20, 30, 15, 80, 100, 10],
+            backgroundColor: '#fff',
+            borderColor: '#FF8D1C',
+            pointRadius: 10,
+            pointHoverRadius: 15
+          },
+          {
+            type: 'bar',
+            label: 'Bar Dataset',
             backgroundColor: '#f87979',
-            data: [40]
+            borderRadius: 100,
+            data: [40, 20, 30, 15, 80, 100, 10]
+          },
+          {
+            type: 'bar',
+            label: 'Bar Dataset 2',
+            backgroundColor: '#ff0',
+            borderRadius: 100,
+            data: [40, 20, 30, 15, 80, 100, 10]
+          },
+          {
+            type: 'bar',
+            label: 'Bar Dataset 3',
+            backgroundColor: '#00f',
+            borderRadius: 100,
+            data: [40, 20, 30, 15, 80, 100, 10]
           }
         ]
   })
--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,8 +1,8 @@
 <script setup>
   import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
 
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement)
 
   defineProps({
     chartData: {
