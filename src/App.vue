6fe6bd7cba8335f96d7f7f49b60daf5c91fac924
Michał Wolny <michal.wolny@it-solve.pl>
2023-09-29 09:10:21 +0200

    zmiany

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
+  import { ref } from 'vue'
+  import BarChart from './components/BarChart.vue'
 
+  const chartOptions = ref({
+    responsive: true
+  })
+
+  const chartData = ref({
+     labels: [ 'January', 'February', 'March'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40]
+          }
+        ]
+  })
 </script>
 
 <template>
-  test
+  <BarChart :chartOptions="chartOptions" :chartData="chartData"/>
 </template>
 
 <style scoped>
